dc695a2a7b3efc1c5b199dde90856135a0ed0c76
Felipe Rosa <flp.far@gmail.com>
2020-09-14 09:30:31 -0300

    Add login page with api call using axios

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function fetchData(user) {
+    try {
+      const response = await axios.post(`http://localhost:3001/api/login`, user);
+      console.log(response)
+    } catch (error) {
+
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const user = { user: {email, password}}
+    fetchData(user);
+  }
+
   return (
-    <h1>Hello, Login</h1>
+    <div className="">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+        <input type="password" name="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
