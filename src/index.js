a78f5167f3da07b68314e47aa7b286baaa37144c
Felipe Rosa <flp.far@gmail.com>
2020-09-17 17:20:50 -0300

    Add global styles

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import './index.css';
 import App from './App';
+import './assets/styles/reset.css';
+import './assets/styles/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
