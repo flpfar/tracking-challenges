c13bdf6ab39163a5c49bb12aae98ac0e03546ddd
Felipe Rosa <flp.far@gmail.com>
2020-09-17 17:21:11 -0300

    Add style to navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { TrendingUp, PieChart, User, HelpCircle } from 'react-feather';
 import styles from './styles.module.css';
 
 const Navbar = () => {
   const navlinks = [
-    {name: 'Track.it', path: '/'},
-    {name: 'Progress', path: '/progress'},
-    {name: 'Profile', path: '/profile'},
-    {name: 'About', path: '/about'}
+    { name: 'Track.it', path: '/', icon: <TrendingUp /> },
+    { name: 'Progress', path: '/progress', icon: <PieChart /> },
+    { name: 'Profile', path: '/profile', icon: <User /> },
+    { name: 'About', path: '/about', icon: <HelpCircle /> }
   ];
 
   return (
-    <nav>
+    <nav className={styles.Navbar}>
       <ul>
         { navlinks.map(link => (
           <li key={link.name}>
             <NavLink to={link.path} exact activeClassName={styles.active}>
+              <span className={styles.arrowDown}></span>
+              {link.icon}
               {link.name}
             </NavLink>
           </li>)
