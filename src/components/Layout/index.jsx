1e5d7d2917aa8f3eb6ba3801d2470c1b4509c1cd
Felipe Rosa <flp.far@gmail.com>
2020-09-18 16:14:48 -0300

    Fix linter issues in Layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Navbar from '../Navbar';
 import Header from '../Header';
 import Footer from '../Footer';
 
-const Layout = ({ current, children, footer }) => {
-  return (
-    <>
-      <section>
-        <Header title={current} />
-        <div className="container">
-          { children }
-        </div>
-      </section>
-      { footer ? <Footer /> : <Navbar />}
-    </>
-  );
+const Layout = ({ current, footer, children }) => (
+  <>
+    <section>
+      <Header title={current} />
+      <div className="container">
+        { children }
+      </div>
+    </section>
+    { footer ? <Footer /> : <Navbar />}
+  </>
+);
+
+Layout.propTypes = {
+  current: PropTypes.string.isRequired,
+  footer: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+};
+
+Layout.defaultProps = {
+  footer: false,
 };
 
 export default Layout;
