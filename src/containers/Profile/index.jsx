4a509a46282622d07fbb1fd94c16090691973c6c
Felipe Rosa <flp.far@gmail.com>
2020-09-18 16:43:40 -0300

    Fix linter issues in Profile

--- src/containers/Profile/index.jsx
+++ src/containers/Profile/index.jsx
@@ -4,27 +4,26 @@ import { logout, updateGoal } from '../../actions/user';
 import Layout from '../../components/Layout';
 import styles from './styles.module.css';
 
-
 const Profile = () => {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.userData.user )
+  const user = useSelector(state => state.userData.user);
   const [inputGoal, setInputGoal] = useState(user.daily_goal);
 
-  function handleGoalSubmit(e){
+  function handleGoalSubmit(e) {
     e.preventDefault();
     dispatch(updateGoal(inputGoal));
   }
 
-  function handleAddOrSubtractButtonClick(e){
+  function handleAddOrSubtractButtonClick(e) {
     const operator = e.target.value;
-    if(operator === '-') {
-      setInputGoal(inputGoal - 1)
+    if (operator === '-') {
+      setInputGoal(inputGoal - 1);
     } else {
-      setInputGoal(inputGoal + 1)
+      setInputGoal(inputGoal + 1);
     }
   }
 
-  function handleLogout(){
+  function handleLogout() {
     dispatch(logout());
   }
 
@@ -33,19 +32,32 @@ const Profile = () => {
       <div className={styles.ProfileContainer}>
         <div className={styles.userInfo}>
           <p className={styles.userName}>{user.name}</p>
-          <p>Email: <span>{user.email}</span></p>
-          <p>Days worked: <span>{user.total_working_days}</span></p>
-          <p>Total challenges: <span>{user.total_challenges}</span></p>
+          <p>
+            Email:
+            <span>{user.email}</span>
+          </p>
+          <p>
+            Days worked:
+            <span>{user.total_working_days}</span>
+          </p>
+          <p>
+            Total challenges:
+            <span>{user.total_challenges}</span>
+          </p>
+          <p>
+            Daily goal:
+            <span>{user.daily_goal}</span>
+          </p>
         </div>
         <div className={styles.goalFormContainer}>
           <h3>Daily goal</h3>
           <form onSubmit={handleGoalSubmit}>
             <div className={styles.goalController}>
-              <button type="button" className={[styles.addSubBtn, styles.subBtn].join(' ')} value="-" onClick={handleAddOrSubtractButtonClick} disabled={ inputGoal <= 1 ? true : false }>-</button>
-              <input type="number" value={inputGoal} min="1" onChange={(e) => setInputGoal(parseInt(e.target.value))} required />
-              <button type="button" className={[styles.addSubBtn, styles.addBtn].join(' ')} value="+" onClick={handleAddOrSubtractButtonClick} >+</button>
+              <button type="button" className={[styles.addSubBtn, styles.subBtn].join(' ')} value="-" onClick={handleAddOrSubtractButtonClick} disabled={inputGoal <= 1}>-</button>
+              <input type="number" value={inputGoal} min="1" onChange={e => setInputGoal(parseInt(e.target.value, 10))} required />
+              <button type="button" className={[styles.addSubBtn, styles.addBtn].join(' ')} value="+" onClick={handleAddOrSubtractButtonClick}>+</button>
             </div>
-            <button type="submit" disabled={user.daily_goal !== inputGoal ? false : true } >Set goal</button>
+            <button type="submit" disabled={user.daily_goal === inputGoal}>Set goal</button>
           </form>
         </div>
         <div className={styles.logoutContainer}>
